4c66cb8d47a008b6d85f26bc7c553b5a9d5daf3b
Alan Alencar <alanalencar@me.com>
2025-06-19 11:20:39 -0300

    Initial Commit

---
+++ Film Catalog/Film_CatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  Film_CatalogApp.swift
+//  Film Catalog
+//
+//  Created by Alan Alencar on 19/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Film_CatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
