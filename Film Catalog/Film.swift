5c7232ba689320e01ad238fca8ff824b48db9d57
Alan Alencar <alanalencar@me.com>
2025-06-20 21:45:41 -0300

    Commit com as funcionalidades de mapeamento (model) do json contendo os dados dos filmes, o arquivo de leitura e criação dos elementos do tipo struct que define os atributos dos filmes.

---
+++ Film Catalog/Film.swift
@@ -0,0 +1,23 @@
+//
+//  Film.swift
+//  Film Catalog
+//
+//  Created by Alan Alencar on 20/06/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct Film: Hashable, Codable {
+    var id: Int
+    var title: String
+    var rated: Int
+    var director: String
+    var actors: String
+    var synopsis: String
+    
+    private var cover: String
+    var poster: Image {
+        Image(cover)
+    }
+}
---
+++ Film Catalog/FilmRow.swift
@@ -0,0 +1,21 @@
+//
+//  FilmRow.swift
+//  Film Catalog
+//
+//  Created by Alan Alencar on 20/06/25.
+//
+
+import SwiftUI
+
+struct FilmRow: View {
+    
+    var film: Film
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FilmRow(film: films[0])
+}
